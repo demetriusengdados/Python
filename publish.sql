a801a3d8bb7cb7ff9095e7b3945226b2033aaf06
Demetrius Magela da Mata <63015481+demetriusengdados@users.noreply.github.com>
2022-06-17 09:18:55 -0300

    Add files via upload

--- publish.sql
+++ publish.sql
@@ -18,19 +18,11 @@ TIPOS:
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
 
 */
-CREATE VIEW basedosdados-dev.br_geobr_mapas.setor_censitario_2010 AS
+
+CREATE VIEW basedosdados-dev.br_ggb_relatorio_lgbtqi.raca_cor AS
 SELECT 
-SAFE_CAST(id_uf AS STRING) id_uf,
-SAFE_CAST(estado_abrev AS STRING) sigla_uf,
-SAFE_CAST( SAFE_CAST( SAFE_CAST(id_municipio AS FLOAT64) AS INT64) AS STRING) id_municipio, -- corrige ponto decimal
-SAFE_CAST(nome_municipio AS STRING) nome_municipio,
-SAFE_CAST(id_distrito AS STRING) id_distrito,
-SAFE_CAST(nome_distrito AS STRING) nome_distrito,
-SAFE_CAST(id_subdistrito AS STRING) id_subdistrito,
-SAFE_CAST(nome_subdistrito AS STRING) nome_subdistrito,
-SAFE_CAST(id_vizinhanca AS STRING) nome_vizinhanca, -- invertida com nome_vizinhanca
-SAFE_CAST( SAFE_CAST( SAFE_CAST(nome_vizinhanca AS FLOAT64) AS INT64) AS STRING) id_vizinhanca, -- invertida com id_vizinhanca e corrige ponto decimal
-SAFE_CAST(id_setor_censitario AS STRING) id_setor_censitario,
-SAFE_CAST(zona AS STRING) zona,
-SAFE.ST_GEOGFROMTEXT(geometria) geometria
-from basedosdados-dev.br_geobr_mapas_staging.setor_censitario_2010 as t
+SAFE_CAST(ano AS STRING) ano,
+SAFE_CAST(raca_cor AS STRING) raca_cor,
+SAFE_CAST(homicidios AS INT64) homicidios,
+SAFE_CAST(prop_homicidios_total AS FLOAT64) prop_homicidios_total
+from basedosdados-dev.br_ggb_relatorio_lgbtqi_staging.raca_cor as t
