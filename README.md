960af3f2b4884ca00eeb9e617e27379bd7ed7fec
Demetrius Magela da Mata <63015481+demetriusengdados@users.noreply.github.com>
2022-06-17 09:05:30 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
+Como capturar os dados de br_cgu_pessoal_executivo_federal?
 
-Diretórios de data e tempo para padronização na Base dos Dados.
+1. Para capturar esses dados, basta verificar o link dos dados originais indicado em `dataset_config.yaml` no item `website`.
 
-Os dados publicados estão disponíveis em: https://basedosdados.org/dataset/br-bd-diretorios-data-tempo.
+2. Caso tenha sido utilizado algum código de captura ou tratamento, estes estarão contidos em `code/`. Se o dado publicado for em sua versão bruta, não existirá a pasta `code/`.
+
+Os dados publicados estão disponíveis em: https://basedosdados.org/dataset/br-cgu-pessoal-executivo-federal
--- publish.sql
+++ publish.sql
@@ -18,8 +18,29 @@ TIPOS:
     - Mais detalhes: https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
 
 */
-CREATE VIEW basedosdados-dev.br_bd_diretorios_data_tempo.trimestre AS
+
+CREATE VIEW basedosdados-dev.br_cgu_pessoal_executivo_federal.terceirizados AS
 SELECT 
-SAFE_CAST(trimestre AS INT64) trimestre,
-SAFE_CAST(semestre AS INT64) semestre
-FROM basedosdados-dev.br_bd_diretorios_data_tempo_staging.trimestre AS t
+SAFE_CAST(ano AS INT64) ano,
+SAFE_CAST(mes AS INT64) mes,
+SAFE_CAST(id_terceirizado AS STRING) id_terceirizado,
+SAFE_CAST(sigla_orgao_superior_unidade_gestora AS STRING) sigla_orgao_superior_unidade_gestora,
+SAFE_CAST(codigo_unidade_gestora AS STRING) codigo_unidade_gestora,
+SAFE_CAST(unidade_gestora AS STRING) unidade_gestora,
+SAFE_CAST(sigla_unidade_gestora AS STRING) sigla_unidade_gestora,
+SAFE_CAST(contrato_empresa AS STRING) contrato_empresa,
+SAFE_CAST(cnpj_empresa AS STRING) cnpj_empresa,
+SAFE_CAST(razao_social_empresa AS STRING) razao_social_empresa,
+SAFE_CAST(cpf AS STRING) cpf,
+SAFE_CAST(nome AS STRING) nome,
+SAFE_CAST(categoria_profissional AS STRING) categoria_profissional,
+SAFE_CAST(nivel_escolaridade AS STRING) nivel_escolaridade,
+SAFE_CAST(quantidade_horas_trabalhadas_semanais AS STRING) quantidade_horas_trabalhadas_semanais,
+SAFE_CAST(unidade_trabalho AS STRING) unidade_trabalho,
+SAFE_CAST(valor_mensal AS STRING) valor_mensal,
+SAFE_CAST(custo_mensal AS STRING) custo_mensal,
+SAFE_CAST(sigla_orgao_trabalho AS STRING) sigla_orgao_trabalho,
+SAFE_CAST(nome_orgao_trabalho AS STRING) nome_orgao_trabalho,
+SAFE_CAST(codigo_siafi_trabalho AS STRING) codigo_siafi_trabalho,
+SAFE_CAST(codigo_siape_trabalho AS STRING) codigo_siape_trabalho
+from basedosdados-dev.br_cgu_pessoal_executivo_federal_staging.terceirizados as t
